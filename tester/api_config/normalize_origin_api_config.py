2aa24f40853de9a13e7302c7b55c513439e882e7
Wang Huan <wanghuan29@baidu.com>
2025-02-20 11:52:03 +0800

    refine

--- tester/api_config/normalize_origin_api_config.py
+++ tester/api_config/normalize_origin_api_config.py
@@ -1,5 +1,5 @@
 
-with open("/data/OtherRepo/PaddleAPITest/tester/api_config/api_config.txt.1", "r") as f:
+with open("/host_home/wanghuan29/APItest3/PaddleAPITest/PaddleNLP_api/unittest_api_config.txt", "r") as f:
     origin_configs = f.readlines()
     f.close()
 
@@ -9,7 +9,7 @@ for config in origin_configs:
     configs.add(config)
 
 
-with open("/data/OtherRepo/PaddleAPITest/tester/api_config/api_config_PaddleX.txt", "w") as f:
+with open("/host_home/wanghuan29/APItest3/PaddleAPITest/tester/api_config/api_config_PaddleNLP_unittest.txt", "w") as f:
     for config in sorted(configs):
         f.write(config)
     f.close()
