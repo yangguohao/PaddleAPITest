cc614dcd1c6e4068d8cc9433e6cc720e48d6fdef
Cutelemon6 <cutelemon6@qq.com>
2025-04-03 08:07:00 +0000

    fix kwarg name=str with spaces in str

--- tester/api_config/config_analyzer.py
+++ tester/api_config/config_analyzer.py
@@ -810,8 +810,11 @@ class APIConfig:
             value, offset = self.get_numpy_type(config, offset)
         elif tocken == "nan":
             value = float('nan')
-        elif config[offset] == '\"':
-            value = tocken
+        elif config[offset - len(tocken) - 1] == '\"':
+            # fix tocken is not correct in str with spaces
+            next_quote_idx  = config.index("\"", offset + 1)
+            value = config[offset - len(tocken):next_quote_idx]
+            offset = next_quote_idx
         elif tocken is None:
             return None, None
         else:
