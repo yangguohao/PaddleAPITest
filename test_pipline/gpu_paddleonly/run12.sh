01aff5f3928ed4b6a6daa4e51f6afd50120530fc
yuwu46 <616473418@qq.com>
2025-04-24 01:49:48 +0000

    refine

--- test_pipline/gpu_paddleonly/run12.sh
+++
@@ -1,3 +0,0 @@
-export CUDA_VISIBLE_DEVICES=3
-export FLAGS_use_system_allocator=1
-for i in {1..10000}; do python engine.py --api_config_file=/home/Test/PaddleAPITest/tester/api_config/api_config_merged_12.txt --paddle_only=True >> /home/Test/PaddleAPITest/test_pipline/paddle_only/paddleonly_12.log 2>&1; done
