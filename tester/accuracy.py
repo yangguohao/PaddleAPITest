4584292e00b52a21f9766a059cd9d6a88104a5d6
Cutelemon6 <cutelemon6@qq.com>
2025-05-06 04:23:55 +0000

    fix broadcast_shape and move config

--- tester/accuracy.py
+++ tester/accuracy.py
@@ -1,6 +1,6 @@
 import gc
 import traceback
-
+import numpy
 import paddle
 import torch
 from func_timeout import func_set_timeout
@@ -258,7 +258,9 @@ class APITestAccuracy(APITestBase):
                 write_to_log("accuracy_error", self.api_config.config)
                 return
             for i in range(len(paddle_output)):
-                if not isinstance(paddle_output[i], paddle.Tensor):
+                if isinstance(paddle_output[i], int):
+                    self.np_assert_accuracy(numpy.array(paddle_output[i]), numpy.array(torch_output[i]), 1e-2, 1e-2, self.api_config)
+                elif not isinstance(paddle_output[i], paddle.Tensor):
                     print("[not compare] ", paddle_output[i], torch_output[i], flush=True)
                     write_to_log("accuracy_error", self.api_config.config)
                     return
@@ -331,7 +333,9 @@ class APITestAccuracy(APITestBase):
                     write_to_log("accuracy_error", self.api_config.config)
                     return
                 for i in range(len(paddle_out_grads)):
-                    if not isinstance(paddle_out_grads[i], paddle.Tensor):
+                    if isinstance(paddle_out_grads[i], int):
+                        self.np_assert_accuracy(numpy.array(paddle_out_grads[i]), numpy.array(torch_out_grads[i]), 1e-2, 1e-2, self.api_config)
+                    elif not isinstance(paddle_out_grads[i], paddle.Tensor):
                         print("[not compare] ", paddle_out_grads[i], torch_out_grads[i], flush=True)
                         write_to_log("accuracy_error", self.api_config.config)
                         return
