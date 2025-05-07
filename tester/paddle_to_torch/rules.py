eb0baa13be8ac425fdca288e598adf5548e8867b
chenhuangrun <1181749441@qq.com>
2025-05-07 06:06:03 +0000

    fix

--- tester/paddle_to_torch/rules.py
+++ tester/paddle_to_torch/rules.py
@@ -1361,6 +1361,14 @@ result = torch.abs(lcm)
 
 
 # m
+class Matrix_transposeRule(BaseRule):
+    def apply(self, paddle_api: str) -> ConvertResult:
+        impl = """
+result = x.transpose(-1, -2)
+"""
+        code = impl.splitlines()
+        return ConvertResult.success(paddle_api, code)
+      
 class MedianRule(BaseRule):
     def apply(self, paddle_api: str) -> ConvertResult:
         impl = """
@@ -1589,7 +1597,16 @@ else:
 
 
 # q
-
+class QrRule(BaseRule):
+    def apply(self, paddle_api: str) -> ConvertResult:
+        impl = """
+mode = locals().get('mode', 'reduced')
+result = torch.linalg.qr(x, mode)
+if mode == "r":
+    result = result[1]
+"""
+        code = impl.splitlines()
+        return ConvertResult.success(paddle_api, code, "result")
 
 # r
 class Roi_aignRule(BaseRule):
@@ -1766,9 +1783,28 @@ else:
         code = impl.splitlines()
         return ConvertResult.success(paddle_api, code)
 
+class SlogdetRule(BaseRule):
+    def apply(self, paddle_api: str) -> ConvertResult:
+        impl = """
+result = torch.linalg.slogdet(x)
+result = torch.stack(result,0)
+"""
+        code = impl.splitlines()
+        return ConvertResult.success(paddle_api, code)
 
 # t
-
+class TriangularSolveRule(BaseRule):
+    def apply(self, paddle_api: str) -> ConvertResult:
+        impl = """
+transpose = locals().get('transpose', False)
+upper = locals().get('upper', True)
+unitriangular = locals().get('unitriangular', False)
+if transpose:
+    x = x.t()
+result = torch.linalg.solve_triangular(x,y,upper=upper,left=True,unitriangular=unitriangular)
+"""
+        code = impl.splitlines()
+        return ConvertResult.success(paddle_api, code)
 
 # u
 
