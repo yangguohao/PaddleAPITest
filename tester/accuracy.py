3c79747b2bd7b9b0c4f0362d74b4520f5d18cdf0
cangtianhuang <l1903374751@gmail.com>
2025-05-29 00:37:55 +0800

    add diff uncertain txt

--- tester/accuracy.py
+++ tester/accuracy.py
@@ -349,6 +349,9 @@ class APITestAccuracy(APITestBase):
             if self.api_config.api_name == "paddle.Tensor.__setitem__":
                 torch_out_grads = torch_out_grads[0]
                 paddle_out_grads = paddle_out_grads[0]
+            
+            # All configs that not compared with torch 
+            # should be moved to tester/api_config/5_accuracy/behavior_diff.txt
             if self.api_config.api_name == "paddle.tensordot":
                 paddle_out_grads = paddle_out_grads[:2]
                 
