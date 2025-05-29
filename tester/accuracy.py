d2b868ae99af548f6b9f103426659cbaa6ae2f8f
chen huangrun <1181749441@qq.com>
2025-05-29 10:23:14 +0000

    fix

--- tester/accuracy.py
+++ tester/accuracy.py
@@ -222,6 +222,11 @@ class APITestAccuracy(APITestBase):
         if self.api_config.api_name == "paddle.incubate.nn.functional.fused_rms_norm": 
             paddle_output = paddle_output[0]
 
+        if self.api_config.api_name == "paddle.unique": 
+            if "return_index=True" in self.api_config.config:
+                paddle_output = list(paddle_output)
+                del paddle_output[1]
+
         if isinstance(paddle_output, paddle.Tensor):
             if isinstance(torch_output, torch.Tensor):
                 try:
