425146ef9041b1d0fd287e5e2fa396325c48fd38
cangtianhuang <l1903374751@gmail.com>
2025-06-03 21:25:55 +0800

    decrease num threads

--- run-example.sh
+++ run-example.sh
@@ -2,8 +2,8 @@
 
 # 配置参数
 # NUM_GPUS!=0 时，engineV2 不受外部 "CUDA_VISIBLE_DEVICES" 影响
-FILE_INPUT="tester/api_config/api_config_paddleonly_1.txt"
-# FILE_PATTERN="tester/api_config/api_config_paddleonly_*.txt"
+FILE_INPUT="tester/api_config/5_accuracy/accuracy_1.txt"
+# FILE_PATTERN="tester/api_config/5_accuracy/accuracy_*.txt"
 LOG_DIR="tester/api_config/test_log"
 NUM_GPUS=-1
 NUM_WORKERS_PER_GPU=-1
