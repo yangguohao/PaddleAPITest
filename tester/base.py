172759303d35709077aa4c348784fc87a5ff3936
Cutelemon6 <cutelemon6@qq.com>
2025-06-04 03:45:59 +0000

    check scaled_dot_product_attention config (maybe a random calculation)

--- tester/base.py
+++ tester/base.py
@@ -66,7 +66,7 @@ stochastic_behavior_apis = frozenset(
         "paddle.nn.functional.dropout3d",
         "paddle.nn.functional.feature_alpha_dropout",
         # "paddle.incubate.nn.functional.fused_multi_head_attention", # If parameter "dropout_rate=0.5, attn_dropout_rate=0.5 (default value)" is not equal to 0.0 or 1.0, the result involves random calculation.
-        "paddle.nn.functional.scaled_dot_product_attention",
+        # "paddle.nn.functional.scaled_dot_product_attention", # If parameter "dropout_p=0.0" is not equal to 0.0 or 1.0, the result involves random calculation.
     ]
 )
 
