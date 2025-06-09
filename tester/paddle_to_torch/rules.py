b4ba67d466234027f3ba815d129a9adf453ea357
chen huangrun <1181749441@qq.com>
2025-06-09 08:09:01 +0000

    fix bug

--- tester/paddle_to_torch/rules.py
+++ tester/paddle_to_torch/rules.py
@@ -4642,16 +4642,15 @@ if isinstance(shape, torch.Tensor):
 elif isinstance(shape, tuple):
     shape = list(shape)
 elements = x.numel()
-if elements != 0:
-    for i, s in enumerate(shape):
-        if s == 0:
-            shape[i] = x.shape[i]
-            elements = elements // x.shape[i]
-        elif s != -1:
-            elements = elements // s
-    for i, s in enumerate(shape):
-        if s == -1:
-            shape[i] = elements
+for i, s in enumerate(shape):
+    if s == 0 and elements != 0:
+        shape[i] = x.shape[i]
+        elements = elements // x.shape[i]
+    elif s != -1 and s != 0:
+        elements = elements // s
+for i, s in enumerate(shape):
+    if s == -1:
+        shape[i] = elements
 """
         core = """
 if x.numel() == 0:
