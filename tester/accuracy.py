2fbb461abba66ce0b7bff8b39ef9aba1083653c8
AyaseNana <13659110308@163.com>
2025-06-16 16:03:32 +0800

    fix accuracy

--- tester/accuracy.py
+++ tester/accuracy.py
@@ -380,7 +380,13 @@ class APITestAccuracy(APITestBase):
             if self.api_config.api_name == "paddle.scale":
                 paddle_out_grads = paddle_out_grads[0]
                 torch_out_grads = torch_out_grads[0]
-                
+            if self.api_config.api_name == "paddle.nn.functional.binary_cross_entropy":
+                paddle_out_grads = paddle_out_grads[0]
+                torch_out_grads = torch_out_grads[0]
+            if self.api_config.api_name == "paddle.nn.functional.binary_cross_entropy_with_logits":
+                paddle_out_grads = paddle_out_grads[0]
+                torch_out_grads = torch_out_grads[0]
+
             if isinstance(paddle_out_grads, paddle.Tensor):
                 if isinstance(torch_out_grads, torch.Tensor):
                     try:
