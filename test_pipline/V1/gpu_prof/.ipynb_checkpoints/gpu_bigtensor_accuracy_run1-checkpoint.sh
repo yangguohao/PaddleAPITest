292261f3efd58cc688c1a2ff7137f270dd925567
zhengshengning <ningzhengsheng@baidu.com>
2025-07-18 11:16:44 +0800

    deleted .ipynb_checkpoints

--- test_pipline/V1/gpu_prof/.ipynb_checkpoints/gpu_bigtensor_accuracy_run1-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-# sleep 3h
-for i in {1..10000}; do python engine.py --api_config_file=test_pipline/V1/gpu_prof/gpu_bigtensor_accuracy_errorconfig_1.txt --paddle_torch_gpu_performance=True >> test_pipline/V1/gpu_prof/gpu_bigtensor_accuracy_errorconfig_1.log 2>&1; done
