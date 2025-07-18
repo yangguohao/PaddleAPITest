925112e061fdfcc1b56b15da87a83af9587ee306
zhengshengning <ningzhengsheng@baidu.com>
2025-07-18 11:12:35 +0800

    add gpu_prof test file

---
+++ test_pipline/V1/gpu_prof_20250717/.ipynb_checkpoints/gpu_bigtensor_accuracy_run3-checkpoint.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=2
+# sleep 3h
+for i in {1..10000}; do python engine.py --api_config_file=test_pipline/gpu_bigtensor/gpu_bigtensor_accuracy/gpu_bigtensor_accuracy_errorconfig_3.txt --paddle_torch_gpu_performance=True >> test_pipline/gpu_bigtensor/gpu_bigtensor_accuracy/gpu_bigtensor_accuracy_errorconfig_3.log 2>&1; done
---
+++ test_pipline/V1/gpu_prof_20250717/rm_all.sh
@@ -0,0 +1,2 @@
+rm -rf tester/api_config/test_log/*
+rm -rf test_pipline/gpu_bigtensor/gpu_bigtensor_accuracy/gpu_bigtensor_accuracy_errorconfig_*.log
