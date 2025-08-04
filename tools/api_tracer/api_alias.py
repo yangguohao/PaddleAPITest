1804f23c06623f17adb3eb64aa1438b75a367636
cangtianhuang <l1903374751@gmail.com>
2025-08-04 08:17:58 +0800

    update api_alias

--- tools/api_tracer/api_alias.py
+++ tools/api_tracer/api_alias.py
@@ -1,5 +1,8 @@
+import os
 import re
 from pathlib import Path
+
+import yaml
 
 INPUT_DIR = Path("tools/api_tracer/trace_output_tmp")
 
@@ -12,7 +15,7 @@ def parse_api(api):
     return api
 
 
-def process_file(input_path):
+def process_file(input_path, target_apis):
     input_name = input_path.name
     output_name = input_name[4:]
     output_path = input_path.parent / output_name
@@ -24,6 +27,9 @@ def process_file(input_path):
 
     alias_apis = set()
     for api in apis:
+        alias_api = api
+        if alias_api not in target_apis:
+            continue
         alias_apis.add(parse_api(api))
 
     with output_path.open("w") as f:
@@ -32,13 +38,23 @@ def process_file(input_path):
 
 
 def main():
-    input_files = list(INPUT_DIR.glob("api_apis*.txt"))
+    yaml_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "api_list",
+        "torch_api_list.yaml",
+    )
+    target_apis = []
+    with open(yaml_path, "r", encoding="utf-8") as f:
+        target_apis = yaml.safe_load(f)
+    print(f"Loaded {len(target_apis)} target APIs.")
+
+    input_files = list(INPUT_DIR.glob("api_apis.txt"))
     if not input_files:
         print(f"No input files found in {INPUT_DIR}", flush=True)
         return
 
     for input_file in sorted(input_files):
-        process_file(input_file)
+        process_file(input_file, target_apis)
 
 
 if __name__ == "__main__":
